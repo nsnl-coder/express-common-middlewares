384699f63d1f76468a8212ffc385f971234ff57f
nsnhatlong <nsnl.coder@gmail.com>
2023-04-02 07:11:17 -0400

    add warning message

--- src/middlewares/requireRole.ts
+++ src/middlewares/requireRole.ts
@@ -2,6 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 
 const requireRole = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      console.log('Please user requireLogin before requireRole!');
+    }
+
+    if (roles.length === 0) {
+      console.log('Please call requireRole with defined roles!');
+    }
+
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
         status: 'fail',
