c1613431bf18abf3d8c16ff066af139b78f28866
nsnhatlong <nsnl.coder@gmail.com>
2023-04-03 04:58:35 -0400

    1.0.15

--- src/middlewares/routeNotFound.ts
+++ src/middlewares/routeNotFound.ts
@@ -4,6 +4,7 @@ const routeNotFound = (req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({
     status: 'fail',
     message: 'This route is not defined',
+    invalidRoute: req.path,
   });
 };
 
